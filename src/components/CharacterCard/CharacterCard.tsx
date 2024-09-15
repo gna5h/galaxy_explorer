64991106243b6f57bb74cefeae76dfc91052a9a2
gna5h <gg.orlanes@gmail.com>
2024-09-15 21:01:23 +0800

    fix: update card padding

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -13,7 +13,7 @@ export default function CharacterCard() {
   return (
     <VStack alignItems={"flex-start"}>
       <Card maxW="sm" bgColor={"primary.yellow"}>
-        <CardBody padding={2}>
+        <CardBody padding={1.5}>
           <Image
             src="https://images.unsplash.com/photo-1533613220915-609f661a6fe1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3RhciUyMHdhcnN8ZW58MHx8MHx8fDA%3D"
             alt="Green double couch with wooden legs"
