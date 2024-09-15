8bfbb04cc686df01cbbbc30a213e4b6e765e8cef
gna5h <gg.orlanes@gmail.com>
2024-09-15 17:31:27 +0800

    feat: add search component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import Search from "../../components/Search/Search.tsx";
 
 export default function Home() {
   return (
-    <Flex bg={"primary.black"} height="100vh" align="center" justify="center">
-      Home
+    <Flex bg={"primary.black"} height="100vh">
+      <Search />
     </Flex>
   );
 }
