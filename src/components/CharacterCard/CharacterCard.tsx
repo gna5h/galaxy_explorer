01f18e22263c66074f0eb07bd8500d780c11b612
gna5h <gg.orlanes@gmail.com>
2024-09-15 19:13:00 +0800

    fix: update card container and image

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -3,47 +3,30 @@ import {
   CardBody,
   Card,
   Image,
-  Stack,
   Heading,
-  Text,
-  Divider,
   CardFooter,
-  ButtonGroup,
-  Button,
+  VStack,
+  Box,
 } from "@chakra-ui/react";
 
 export default function CharacterCard() {
   return (
-    <Card maxW="sm">
-      <CardBody>
-        <Image
-          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          alt="Green double couch with wooden legs"
-          borderRadius="lg"
-        />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">Living room Sofa</Heading>
-          <Text>
-            This sofa is perfect for modern tropical spaces, baroque inspired
-            spaces, earthy toned spaces and for people who love a chic design
-            with a sprinkle of vintage design.
-          </Text>
-          <Text color="blue.600" fontSize="2xl">
-            $450
-          </Text>
-        </Stack>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <ButtonGroup spacing="2">
-          <Button variant="solid" colorScheme="blue">
-            Buy now
-          </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Add to cart
-          </Button>
-        </ButtonGroup>
-      </CardFooter>
-    </Card>
+    <VStack alignItems={"flex-start"}>
+      <Card maxW="sm" bgColor={"primary.yellow"}>
+        <CardBody padding={2}>
+          <Image
+            src="https://images.unsplash.com/photo-1533613220915-609f661a6fe1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3RhciUyMHdhcnN8ZW58MHx8MHx8fDA%3D"
+            alt="Green double couch with wooden legs"
+            objectFit={"cover"}
+            borderRadius="lg"
+          />
+        </CardBody>
+      </Card>
+      <Box pl={3} bgColor={"primary.black"}>
+        <Heading size="sm" color={"white"}>
+          Living room Sofa
+        </Heading>
+      </Box>
+    </VStack>
   );
 }
