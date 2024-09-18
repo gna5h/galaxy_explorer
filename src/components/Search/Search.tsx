ba5dbf2cf43aa99219e187d6d71efbfb772d64a7
gna5h <gg.orlanes@gmail.com>
2024-09-18 23:03:47 +0800

    fix: update search display responsiveness

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,52 +1,109 @@
 import {
   Stack,
-  Box,
   Heading,
   Input,
   IconButton,
   InputGroup,
   InputLeftElement,
-  useBreakpointValue,
+  VStack,
+  HStack,
+  Collapse,
+  useDisclosure,
+  Divider,
 } from "@chakra-ui/react";
 import { Search2Icon, HamburgerIcon } from "@chakra-ui/icons";
-import React from "react";
+import React, { useState } from "react";
+import { useAppDispatch } from "../../store/store.ts";
+import { searchPeople } from "../../store/thunks/people_thunk.ts";
 
 export default function Search({
   isSmallScreen,
 }: {
   isSmallScreen: boolean | undefined;
 }) {
+  const dispatch = useAppDispatch();
+  const [keyword, setKeyword] = useState("");
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+  const { isOpen, onToggle } = useDisclosure();
+
+  const handleSearch = () => {
+    dispatch(searchPeople(keyword));
+  };
+
   return (
-    <Stack
-      direction="row"
-      justifyContent={"space-between"}
+    <VStack
       alignItems={"center"}
       bg={"primary.yellow"}
-      height={"10vh"}
       width={"100%"}
-      paddingX={isSmallScreen ? "50" : "75"}
+      paddingX={isSmallScreen ? 0 : 50}
+      paddingY={isSmallScreen ? 5 : 0}
     >
-      <Heading size={"lg"}>Galaxy Explorer</Heading>
-      <Stack
-        direction="row"
-        justifyContent={"space-between"}
+      <HStack
+        height={isSmallScreen ? "5vh" : "10vh"}
+        width={"100%"}
         alignItems={"center"}
-        gap={isSmallScreen ? 5 : 10}
+        justifyContent={"space-between"}
+        padding={5}
       >
-        {isSmallScreen ? (
+        <Heading size={"lg"}>Galaxy Explorer</Heading>
+        <Stack
+          direction="row"
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          gap={isSmallScreen ? 5 : 10}
+        >
+          {isSmallScreen ? (
+            <IconButton
+              aria-label="Search"
+              icon={
+                <Search2Icon
+                  color="primary.black"
+                  _hover={{ color: "white" }}
+                />
+              }
+              variant="ghost"
+              _hover={{ backgroundColor: "transparent" }}
+              onClick={onToggle}
+            />
+          ) : (
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Search2Icon color="primary.black" />
+              </InputLeftElement>
+              <Input
+                placeholder="Quick Search"
+                _placeholder={{ color: "primary.black" }}
+                focusBorderColor="white"
+                _hover={{ borderColor: "white" }}
+                borderRadius={100}
+                borderColor={"primary.black"}
+                size="md"
+                onChange={(event) => setKeyword(event.target.value)}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    handleSearch();
+                  }
+                }}
+              />
+            </InputGroup>
+          )}
           <IconButton
-            aria-label="Search"
+            aria-label="Filter"
             icon={
-              <Search2Icon color="primary.black" _hover={{ color: "white" }} />
+              <HamburgerIcon
+                color="primary.black"
+                boxSize={6}
+                _hover={{ color: "white" }}
+              />
             }
             variant="ghost"
             _hover={{ backgroundColor: "transparent" }}
           />
-        ) : (
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Search2Icon color="primary.black" />
-            </InputLeftElement>
+        </Stack>
+      </HStack>
+      {isSmallScreen && (
+        <Collapse in={isOpen} animateOpacity>
+          <HStack height={"5vh"} width={"90vw"} margin={1}>
             <Input
               placeholder="Quick Search"
               _placeholder={{ color: "primary.black" }}
@@ -55,22 +112,16 @@ export default function Search({
               borderRadius={100}
               borderColor={"primary.black"}
               size="md"
+              onChange={(event) => setKeyword(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  handleSearch();
+                }
+              }}
             />
-          </InputGroup>
-        )}
-        <IconButton
-          aria-label="Filter"
-          icon={
-            <HamburgerIcon
-              color="primary.black"
-              boxSize={6}
-              _hover={{ color: "white" }}
-            />
-          }
-          variant="ghost"
-          _hover={{ backgroundColor: "transparent" }}
-        />
-      </Stack>
-    </Stack>
+          </HStack>
+        </Collapse>
+      )}
+    </VStack>
   );
 }
